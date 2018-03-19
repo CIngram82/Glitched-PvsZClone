eca7245853d3d7d68e92e447e29800a37219d86c
CIngram82 <cingram82@gmail.com>
2018-03-19 09:15:41 -0400

    Menu set up and transitions
    Setting up Scenes and transitions. TODO fix fade in/out

---
+++ Assets/Scripts/FadePanel.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadePanel : MonoBehaviour {
+    private Image image;
+    public float fadeInTime;
+    private Color imageColor;
+
+    public float fadeOutTime;
+    public bool fadeIn;
+    public bool fadeOut;
+
+
+
+    private float imageAlpha;
+    private float alphaChange;
+    
+    void Start () {
+        image = GetComponent<Image>();
+	}
+	
+
+    
+	void Update () {
+
+        if (fadeIn)
+        {
+            alphaChange += Time.deltaTime / fadeInTime;
+            imageAlpha = Mathf.Lerp(1f, 0f, alphaChange);
+            imageColor.a -= alphaChange;
+            image.color = imageColor;
+            if (alphaChange >= 1f)
+            {
+                alphaChange = 0;
+                fadeIn = false;
+                gameObject.SetActive(false);
+            }
+        }
+
+        if(!fadeIn && fadeOut)
+        {
+            gameObject.SetActive(true);
+            alphaChange += Time.deltaTime * fadeOutTime;
+            imageAlpha = Mathf.Lerp(0f, 1f, alphaChange);
+            imageColor.a += alphaChange;
+            image.color = imageColor;
+            if (imageAlpha >= 1) fadeIn = false;
+        }
+    }
+}
