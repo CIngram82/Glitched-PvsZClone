8d7288cf87ad81184a197223b75d922a6ec59c94
CIngram82 <cingram82@gmail.com>
2018-03-18 22:31:48 -0400

    Inital Commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNextLevelAfter;
+
+    private void Start()
+    {
+        if(autoLoadNextLevelAfter != 0) Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+    }
+
+    public void LoadLevel(string name){
+        SceneManager.LoadScene(name);
+	}
+
+	public void QuitRequest(){
+		Application.Quit ();
+	}
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+    public AudioClip[] levelMusicArray;
+    private AudioSource audioSource;
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;  
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        AudioClip currentLevelMusic = levelMusicArray[scene.buildIndex];
+        
+        if(currentLevelMusic != null)
+        {
+            audioSource.clip = currentLevelMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+  
+}
