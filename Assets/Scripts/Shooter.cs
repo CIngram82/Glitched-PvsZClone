2870d9bcaffeb04c804a86f9688e7a4cc79423ad
CIngram82 <cingram82@gmail.com>
2018-03-29 17:16:58 -0400

    projectiles and anim tuning
    done with adding projectiles. tuned fire rate of some defenders.

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+    public GameObject projectile, projectileParent, projectileSpawnPoint;
+
+    private void Fire()
+    {
+        GameObject newProjectile = Instantiate(projectile);
+        newProjectile.transform.position = projectileSpawnPoint.transform.position;
+        newProjectile.transform.parent = projectileParent.transform;
+      
+
+    }
+
+
+}
